3e5358b806de602687773ca524f7fccc6d9e4c1c
Adnin03 <adnindanil03@gmail.com>
2025-07-31 07:54:18 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan Git Remote</title>
 </head>
+
 <body>
     <h1>Hello World!</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolorum deleniti nisi earum eligendi ad vero sint? Quae molestias perspiciatis neque fugiat, magnam totam veritatis sapiente consequuntur eveniet. Tempora, nemo.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolorum deleniti nisi earum eligendi ad vero sint?
+        Quae molestias perspiciatis neque fugiat, magnam totam veritatis sapiente consequuntur eveniet. Tempora, nemo.
+    </p>
+    <ul>
+        <li>Siji</li>
+        <li>Loro</li>
+        <li>Telu</li>
+    </ul>
 </body>
+
 </html>
