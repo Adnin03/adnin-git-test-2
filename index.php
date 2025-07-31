bec7f2d9db170a8c8d3166f9d4c13e9666e51e23
Adnin03 <adnindanil03@gmail.com>
2025-07-31 07:44:54 +0700

    menambahkan teks paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolorum deleniti nisi earum eligendi ad vero sint? Quae molestias perspiciatis neque fugiat, magnam totam veritatis sapiente consequuntur eveniet. Tempora, nemo.</p>
 </body>
 </html>
